d067096267070467eb8816b1f38a7fe1192e2d25
77yu77 <17876315884@163.com>
2023-11-14 18:09:43 +0800

    fix:fix shell problem

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ id=$2
 ifconfig eth0:sdneth0 $grobalIP netmask 255.255.255.255 up
 iptables -t nat -A OUTPUT -d 10.233.0.0/16 -j MARK --set-mark $id
 iptables -t nat -A POSTROUTING -m mark --mark $id -d 10.233.0.0/16 -j SNAT --to-source $grobalIP
+chmod 777 link_exporter
 ./link_exporter &
 /podserver
